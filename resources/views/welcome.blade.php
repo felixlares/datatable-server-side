8c278b4b47ad86505b59781471a3430c3148f6d5
Felix Lares <felixlares@gmail.com>
2022-12-21 20:46:22 -0500

    test basico

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
 
         <title>Laravel</title>
 
+        {{-- comentar luego los cdn y probar bien el vite --}}
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"/>
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4-4.6.0/jq-3.6.0/jszip-2.5.0/dt-1.13.1/af-2.5.1/b-2.3.3/b-colvis-2.3.3/b-html5-2.3.3/b-print-2.3.3/cr-1.6.1/date-1.2.0/fc-4.2.1/fh-3.3.1/kt-2.8.0/r-2.4.0/rg-1.3.0/rr-1.3.1/sc-2.0.7/sb-1.4.0/sp-2.1.0/sl-1.5.0/datatables.min.css"/>
          
@@ -17,7 +18,7 @@
 
     </head>
     <body class="antialiased">
-        <div class="card m-5">
+        <div class=" m-5">
             <h1 class="p-2">{{ __('products.products') }}</h1>
             <div class="card-body p-2">
                 <table id="product" class="table table-bordered table-sm table-hover dt-responsive nowrap table-adjust">
@@ -53,7 +54,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                        {{-- <tr>
                             <td> </td>
                             <td> texto de prueba</td>
                             <td> texto de prueba</td>
@@ -139,18 +140,65 @@
                             <td> </td>
                             <td> </td>
                             <td> </td>
-                        </tr>
+                        </tr> --}}
                     </tbody>
                 </table>
             </div>
         </div>     
-          <button id="ok" class="btn btn-primary">OK</button>
+          {{-- <button id="ok" class="btn btn-primary">OK</button> --}}
         <script>
             $(document).ready(function () {
                 $('#product').DataTable({
-                    processing: true,
+                    processing: false,
                     serverSide: true,
-                    ajax: 'scripts/server_processing.php',
+                    ajax: "{{ route('products') }}",
+                    columns: [
+                        { data: 'id', width: '30px', render: function(data, type) { return ''; } },
+                        { data: 'part_number' },
+                        { data: 'name', render: function ( data, type, row, meta ) { return '<a href="javascript:void(0);" onclick="openProduct('+row.id+')">'+data+'</a>'; } },
+                        { name: 'manufacturer', data: 'manufacturer' },
+                        { name: 'category', data: 'category' },
+                        { name: 'warehouse_section', data: 'warehouse_section' },
+                        { data: 'qty_purchased' },
+                        { data: 'qty_booked_in' },
+                        { data: 'qty_due_from_pos' },
+                        { data: 'qty_booked_out' },
+                        { data: 'qty_allocated_to_sos' },
+                        { name: 'qty_in_stock', data: 'qty_in_stock' },
+                        { data: 'qty_available' },
+                        { data: 'qty_available_alert_below' },
+                        { data: 'cost_min' },
+                        { data: 'cost_max' },
+                        { data: 'cost_last' },
+                        { name: 'cost_average', data: 'cost_average' },
+                        { data: 'price_min' },
+                        { data: 'price_max' },
+                        { data: 'price_last' },
+                        { data: 'price_average' },
+                        { name: 'descontinued_flag', data: 'descontinued_flag', render: function ( data, type, row, meta ) { if (data > 0) { return '<span class="px-3 custom-tooltip-text" data-toggle="tooltip" data-placement="bottom" title="{{ __('products.product_descontinued') }}"><img src="./images/products-table/descontinued.png" /></span><span class="d-none">{{ __('global.yes') }}</span>'; } else { return ''; } } },
+                        { name: 'clearance_flag', data: 'clearance_flag', render: function ( data, type, row, meta ) { if (data > 0) { return '<span class="px-3 custom-tooltip-text" data-toggle="tooltip" data-placement="bottom" title="{{ __('products.product_clearance') }}"><img src="./images/products-table/clearance.png" /></span><span class="d-none">{{ __('global.yes') }}</span>'; } else { return ''; } } },
+                        { data: 'in_alert', className: 'never' },
+                        { data: 'product_value', className: 'never' },
+                        { name: 'in_stock', data: 'in_stock', className: 'never' }
+                    ],
+                    columnDefs: [
+                        { orderable: false, targets: 0 },
+                        { orderable: true, targets: 1 },
+                        { responsivePriority: 0, targets: 0 },
+                        { responsivePriority: 1, targets: 2 },
+                        { responsivePriority: 2, targets: 1 },
+                        { responsivePriority: 3, targets: 4 },
+                        { responsivePriority: 4, targets: 5 },
+                        { responsivePriority: 5, targets: 19 },
+                        { responsivePriority: 6, targets: 23 },
+                        { responsivePriority: 7, targets: 13 },
+                        { targets: [5,6,7,9,14,15,18,19,22,23], visible: false },
+                        // { className: 'text-center', targets: [6,7] },
+                        { className: 'dt-body-left', targets: [22,23] },
+                        { className: 'dt-body-right', targets: [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21] },
+                        { orderable: false, visible: false, targets: [24,25] },
+                        { searchable: false, targets: [0,25] }
+                    ],
                 });
             });
         </script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Yajra\DataTables\Facades\DataTables;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,65 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/products', function (Request $request)
+{
+    $productsDB = DB::table('products')
+    ->leftJoin('product_warehouse_sections', 'products.product_warehouse_section_id', '=', 'product_warehouse_sections.id')
+      ->leftJoin('product_manufacturers', 'products.product_manufacturer_id', '=', 'product_manufacturers.id')
+      ->leftJoin('product_categories', 'products.product_categorie_id', '=', 'product_categories.id')
+      ->where('products.deleted_at', NULL)
+      ->get([
+        'products.id',
+        'products.part_number',
+        'products.name',
+        'product_warehouse_sections.name AS warehouse_section',
+        'product_manufacturers.name AS manufacturer',
+        'product_categories.name AS category',
+        'products.descontinued_flag',
+        'products.clearance_flag',
+        'products.qty_purchased',
+        'products.qty_booked_in',
+        'products.qty_due_from_pos',
+        'products.qty_booked_out',
+        'products.qty_allocated_to_sos',
+        'products.qty_in_stock',
+        'products.qty_available',
+        'products.qty_available_alert_below',
+        'products.cost_min',
+        'products.cost_max',
+        'products.cost_last',
+        'products.cost_average',
+        'products.price_min',
+        'products.price_max',
+        'products.price_last',
+        'products.price_average',
+        'products.id AS in_alert',
+        'products.id AS product_value',
+      ]);
+
+      $products = $productsDB->map(function ($item) {
+        $item->in_alert = $item->qty_in_stock < $item->qty_available_alert_below ? 1 : 0;
+        $item->in_stock = $item->qty_in_stock > 0 ? 1 : 0;
+        $item->product_value = $item->qty_in_stock * $item->cost_average;
+        $item->qty_purchased = number_format($item->qty_purchased);
+        $item->qty_booked_in = number_format($item->qty_booked_in);
+        $item->qty_due_from_pos = number_format($item->qty_due_from_pos);
+        $item->qty_booked_out = number_format($item->qty_booked_out);
+        $item->qty_allocated_to_sos = number_format($item->qty_allocated_to_sos);
+        $item->qty_in_stock = number_format($item->qty_in_stock);
+        $item->qty_available = number_format($item->qty_available);
+        $item->qty_available_alert_below = number_format($item->qty_available_alert_below);
+        $item->cost_min = $item->cost_min;
+        $item->cost_max = $item->cost_max;
+        $item->cost_last = $item->cost_last;
+        $item->cost_average = $item->cost_average;
+        $item->price_min = $item->price_min;
+        $item->price_max = $item->price_max;
+        $item->price_last = $item->price_last;
+        $item->price_average = $item->price_average;
+        return $item;
+      });
+    Log::alert(["Hola" => $request->all()]);
+    return DataTables::of($products)->make(true);
+}
+)->name('products');
